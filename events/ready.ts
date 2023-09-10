a690686e1d1befb7e01a04bb8251df17cb2b9cd6
AndrewDragon <andrewdragonch@proton.me>
2023-09-09 20:39:50 -0400

    Added statuses

--- events/ready.ts
+++ events/ready.ts
@@ -10,5 +10,20 @@ export default (client: Client): void => {
         await client.application.commands.set(Commands);
 
         console.log(`${client.user.username} is online`);
+
+        const serverCount = Number.parseFloat(String(client.guilds.cache.size)).toLocaleString("en-US");
+        const channelCount = Number.parseFloat(String(client.channels.cache.size)).toLocaleString("en-US");
+        const userCount = Number.parseFloat(String(client.guilds.cache.reduce((a, g) => a + g.memberCount, 0))).toLocaleString("en-US");
+    
+        const statuses = [
+          { "name": `${serverCount} servers & ${userCount} users and ${channelCount} channels`, "type": 3 },
+          { "name": "\/play", "type": 2 },
+          { "name": "\/help", "type": 0 }
+        ];
+    
+        setInterval(() => {
+          const status = statuses[Math.floor(Math.random() * statuses.length)];
+          client.user?.setActivity(status.name, { type: status.type });
+        }, 60000);
     });
 };
